91998ff404307d70919df7f6d6d11ec5b9d98961
Jakub Dorňák <jakub.dornak@misli.cz>
2018-10-16 18:20:35 +0200

    Bump version: 0.1.1 → 1.0.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 
 setup(
     name='django-template-admin',
-    version='0.1.1',
+    version='1.0.0',
     description='Django application for advanced template management',
     long_description=long_description,
     author='Jakub Dorňák',
