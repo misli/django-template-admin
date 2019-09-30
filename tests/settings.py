8bb9fc8576f083f39eef6f814339bec93f33529a
Jakub Dorňák <jakub.dornak@misli.cz>
2019-09-30 12:37:42 +0200

    update tests/settings.py
    - add MIDDLEWARE
    - dry TEMPLATES

--- tests/settings.py
+++ tests/settings.py
@@ -17,53 +17,50 @@ INSTALLED_APPS = [
     'template_admin',
 ]
 
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
 ROOT_URLCONF = 'tests.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['tests/templates'],
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 if os.environ.get('VIRTUAL_ENV', '').endswith('-cache'):
     # Templates setting with django.template.loaders.cached.Loader
-    TEMPLATES = [
-        {
-            'BACKEND': 'django.template.backends.django.DjangoTemplates',
-            'DIRS': ['tests/templates'],
-            'OPTIONS': {
-                'context_processors': [
-                    'django.template.context_processors.debug',
-                    'django.template.context_processors.request',
-                    'django.contrib.auth.context_processors.auth',
-                    'django.contrib.messages.context_processors.messages',
-                ],
-                'loaders': [
-                    ('template_admin.loader.Loader', [
-                        ('django.template.loaders.cached.Loader', [
-                            'django.template.loaders.filesystem.Loader',
-                            'django.template.loaders.app_directories.Loader',
-                        ]),
-                    ]),
-                ],
-            },
-        },
+    TEMPLATES[0]['OPTIONS']['loaders'] = [
+        ('template_admin.loader.Loader', [
+            ('django.template.loaders.cached.Loader', [
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
+            ]),
+        ]),
     ]
 else:
     # Templates setting without django.template.loaders.cached.Loader
-    TEMPLATES = [
-        {
-            'BACKEND': 'django.template.backends.django.DjangoTemplates',
-            'DIRS': ['tests/templates'],
-            'OPTIONS': {
-                'context_processors': [
-                    'django.template.context_processors.debug',
-                    'django.template.context_processors.request',
-                    'django.contrib.auth.context_processors.auth',
-                    'django.contrib.messages.context_processors.messages',
-                ],
-                'loaders': [
-                    ('template_admin.loader.Loader', [
-                        'django.template.loaders.filesystem.Loader',
-                        'django.template.loaders.app_directories.Loader',
-                    ]),
-                ],
-            },
-        },
+    TEMPLATES[0]['OPTIONS']['loaders'] = [
+        ('template_admin.loader.Loader', [
+            'django.template.loaders.filesystem.Loader',
+            'django.template.loaders.app_directories.Loader',
+        ]),
     ]
 
 # Database
